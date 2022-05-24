d167a63235a5c40bbf985778b451bb3821990569
Lissandro Ijanc <lissandro1@gmail.com>
2022-05-24 20:22:33 -0300

    update findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,10 +13,13 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
 		
-		Seller sl = sellerDao.findById(3);
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 		
-		System.out.println(sl);
 
 	}
 
